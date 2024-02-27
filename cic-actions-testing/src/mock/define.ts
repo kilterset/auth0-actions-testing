8f4c2c313c82ad2deaa06de5a0d80c96188b56ac
Pete Nicholls <pnicholls@uncommonpurpose.com>
2024-02-27 14:31:08 +1300

    Support transient attributes as second factory arg

--- cic-actions-testing/src/mock/define.ts
+++ cic-actions-testing/src/mock/define.ts
@@ -4,7 +4,17 @@ import { Factory } from "fishery";
  * Define a new mock factory. This will return the Fishery factory's `build`
  * function.
  */
-export function define<T>(...args: Parameters<typeof Factory.define<T>>) {
-  const factory = Factory.define<T>(...args);
-  return factory.build.bind(factory);
+export function define<T, I = any>(
+  ...args: Parameters<typeof Factory.define<T, I>>
+) {
+  const factory = Factory.define<T, I>(...args);
+
+  return (
+    attributes?: Parameters<typeof factory.build>[0],
+    transient?: Parameters<typeof factory.transient>[0]
+  ) => {
+    return factory.build(attributes as Parameters<typeof factory.build>[0], {
+      transient,
+    });
+  };
 }
