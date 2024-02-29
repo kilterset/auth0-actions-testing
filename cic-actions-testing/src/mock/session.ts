6fb56c5975dd395fe578b3a67c4acdef78db30b6
Pete Nicholls <pnicholls@uncommonpurpose.com>
2024-03-01 08:58:10 +1300

    session mock

--- cic-actions-testing/src/mock/session.ts
+++ cic-actions-testing/src/mock/session.ts
@@ -1,12 +1,16 @@
 import OktaCIC from "../types";
+import { chance } from "./chance";
 import { define } from "./define";
 
 export const session = define<OktaCIC.Session>(() => {
   return {
-    id: "sess_123fake",
+    id: `sess_${chance.hash({ length: 10 })}`,
     device: {
-      last_asn: "AS13335",
-      last_ip: "0.0.0.0",
+      // AS numbers, or ASNs, are unique 16 bit numbers between 1 and 65534 or
+      // 32 bit numbers between 131072 and 4294967294. They are presented in
+      // this format: AS(number).
+      last_asn: `AS${Math.floor(Math.random() * 65534)}`,
+      last_ip: chance.ip(),
     },
   };
 });
