d726958944391617a421d0b06bf9ce65d10064c8
Pete Nicholls <pnicholls@uncommonpurpose.com>
2024-03-01 08:36:31 +1300

    organization mock

--- cic-actions-testing/src/mock/organization.ts
+++ cic-actions-testing/src/mock/organization.ts
@@ -1,11 +1,19 @@
 import OktaCIC from "../types";
+import { chance } from "./chance";
 import { define } from "./define";
 
-export const organization = define<OktaCIC.Organization>(() => {
+export const organization = define<OktaCIC.Organization>(({ params }) => {
+  const display_name = params.display_name || chance.company();
+
+  const name = display_name
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
+
   return {
-    display_name: "My Organization",
-    id: "org_juG7cAQ0CymOcVpV",
+    display_name,
+    name,
+    id: `org_${chance.string({ alpha: true, length: 16 })}`,
     metadata: {},
-    name: "my-organization",
   };
 });
