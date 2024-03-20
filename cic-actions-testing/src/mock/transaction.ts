df5e47977822b9cff21d829dc8094cbbafd8131e
Pete Nicholls <pnicholls@uncommonpurpose.com>
2024-03-21 10:26:43 +1300

    Transaction mock

--- cic-actions-testing/src/mock/transaction.ts
+++ cic-actions-testing/src/mock/transaction.ts
@@ -1,24 +1,42 @@
 import OktaCIC from "../types";
+import { chance } from "./chance";
 import { define } from "./define";
 
-export const transaction = define<OktaCIC.Transaction>(() => {
+export const transaction = define<OktaCIC.Transaction>(({ params }) => {
+  const locale = params.locale || chance.locale();
+
   return {
     acr_values: [],
-    locale: "en",
+    locale,
+    ui_locales: [locale],
     requested_scopes: [],
-    ui_locales: ["en"],
-    protocol: "oidc-basic-profile",
-    redirect_uri: "http://someuri.com",
+    protocol: chance.pickone([
+      "oidc-basic-profile",
+      "oidc-implicit-profile",
+      "oauth2-device-code",
+      "oauth2-resource-owner",
+      "oauth2-resource-owner-jwt-bearer",
+      "oauth2-password",
+      "oauth2-access-token",
+      "oauth2-refresh-token",
+      "oauth2-token-exchange",
+      "oidc-hybrid-profile",
+      "samlp",
+      "wsfed",
+      "wstrust-usernamemixed",
+    ]),
+    redirect_uri: chance.url({ path: "redirect" }),
     prompt: ["none"],
-    login_hint: "test@test.com",
-    response_mode: "form_post",
-    response_type: ["id_token"],
-    state: "AABBccddEEFFGGTTasrs",
-    requested_authorization_details: [
-      {
-        type: "foo",
-      },
-    ],
-    linking_id: "abc_dynamic_linking_id_123",
+    login_hint: chance.pickone([chance.email(), undefined]),
+    response_mode: chance.pickone([
+      "query",
+      "fragment",
+      "form_post",
+      "web_message",
+    ]),
+    response_type: [chance.pickone(["code", "token", "id_token"])],
+    state: chance.string({ length: 32, alpha: true, numeric: true }),
+    requested_authorization_details: [],
+    linking_id: chance.string({ length: 32, alpha: true, numeric: true }),
   };
 });
