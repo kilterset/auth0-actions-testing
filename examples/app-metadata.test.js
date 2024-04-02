120bb9ed1a12f6daea5c15f83f44b61d0be1a667
Pete Nicholls <pnicholls@kilterset.com>
2024-04-03 10:29:46 +1300

    Add examples (and run them in CI)

---
+++ examples/app-metadata.test.js
@@ -0,0 +1,53 @@
+const test = require("node:test");
+const { ok, strictEqual } = require("node:assert");
+const { onExecutePostLogin } = require("./app-metadata");
+
+const {
+  actionTestSetup,
+} = require("@kilterset/auth0-actions-testing/node-test-runner");
+
+test("onExecutePostLogin", async (t) => {
+  const { auth0 } = await actionTestSetup(t);
+
+  await t.test("records a lucky number", async () => {
+    const action = auth0.mock.actions.postLogin({
+      secrets: { MAX_LUCKY_NUMBER: 42 },
+      user: auth0.mock.user({
+        app_metadata: {},
+      }),
+    });
+
+    await action.simulate(onExecutePostLogin);
+
+    const { lucky_number } = action.user.app_metadata;
+
+    ok(
+      typeof lucky_number === "number",
+      `Expected the user's lucky number to be a number (got ${lucky_number})`
+    );
+
+    ok(
+      lucky_number < 42,
+      `Expected lucky number not to exceed the maximum allowed (got ${lucky_number})`
+    );
+  });
+
+  await t.test("does not overwrite lucky numbers", async () => {
+    const action = auth0.mock.actions.postLogin({
+      secrets: { MAX_LUCKY_NUMBER: 42 },
+      user: auth0.mock.user({
+        app_metadata: { lucky_number: 17 },
+      }),
+    });
+
+    await action.simulate(onExecutePostLogin);
+
+    const { lucky_number } = action.user.app_metadata;
+
+    strictEqual(
+      lucky_number,
+      17,
+      `Expected the user's lucky number to be unchanged`
+    );
+  });
+});
