a182e93de07fe2cf65781c8f578e70fbb5beebf8
Pete Nicholls <pnicholls@kilterset.com>
2024-04-03 15:33:10 +1300

    Explain app metadata example

--- examples/app-metadata.js
+++ examples/app-metadata.js
@@ -1,3 +1,7 @@
+/**
+ * Shows how to set app metadata for a user after they log in, provided it
+ * hasn't already been set.
+ */
 exports.onExecutePostLogin = async (event, api) => {
   if (event.user.app_metadata["lucky_number"]) {
     // Do nothing. User already has a lucky number.
