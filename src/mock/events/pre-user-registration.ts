3aa7935fc0776aaead7fe0ee52b93483db73c311
Pete Nicholls <pnicholls@kilterset.com>
2024-04-11 10:09:49 +1200

    Use chance.auth0().tenantId() consistently

--- src/mock/events/pre-user-registration.ts
+++ src/mock/events/pre-user-registration.ts
@@ -12,7 +12,7 @@ import { identity } from "../identity";
 
 export const preUserRegistration = define<Auth0.Events.PreUserRegistration>(
   ({ params }) => {
-    const tenantId = params.tenant?.id || chance.n(chance.word, 2).join("-");
+    const tenantId = params.tenant?.id || chance.auth0().tenantId();
     const hostname = params.request?.hostname || `${tenantId}.auth0.com`;
 
     const connectionValue = params.connection
