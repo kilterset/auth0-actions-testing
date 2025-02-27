175f29cb347d4544771b8b1d0c9b5cb63600f708
Jason Bartz <jason.bartz@covetrus.com>
2025-02-27 09:41:26 -0500

    First commit, changes only

--- src/test-runners/node-test-runner/fetch-mock.ts
+++ src/test-runners/node-test-runner/fetch-mock.ts
@@ -1,8 +1,8 @@
 import { TestContext } from "./test-context";
-import { polyfillFetch } from "./polyfill-fetch";
+// import { polyfillFetch } from "./polyfill-fetch";
 
 export async function configureFetchMock(testContext: TestContext) {
-  await polyfillFetch(testContext);
+  // await polyfillFetch(testContext);
   const { default: fetchMock } = await import("fetch-mock");
   testContext.afterEach(fetchMock.restore);
   return fetchMock;
